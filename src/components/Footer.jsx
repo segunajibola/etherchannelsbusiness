d7e6b86540330615709093bb632ed953223e65ea
Segun Ajibola <ajbl.segun@gmail.com>
2022-11-12 00:55:23 +0100

    fixTestimonials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,42 +16,43 @@ import { HashLink } from 'react-router-hash-link';
 
 const Footer = () => {
   return (
-    <footer className='w-full text-center text-gray-300 bg-[#2b4b77] md:pt-10 dark:bg-gray-300 dark:text-gray-800'>
+    <footer className='w-full text-left text-gray-300 bg-[#2b4b77] md:pt-10 dark:bg-gray-300 dark:text-gray-800'>
         <div className='border-t-2 border-gray-300 dark:border-gray-500 border-solid'></div>
-        <div className='p-4'>
-            <div className='mx-auto grid max-w-7xl grid-cols-2 gap-6 space-y-4 md:mx-2 md:grid-cols-2 '>
-                <div className='mx-auto'>
-                    <h5 className='mt-4 text-lg font-semibold'>
+            <div className='grid max-w-7xl md:grid-cols-2 gap-1 space-y-1 md:mx-2 border-4 border-red-500 p-2'>
+                <div className=''>
+                    <h5 className='text-xl underline font-semibold'>
                        Quick Links
                     </h5>
                     <p>
                         <HashLink className="cursor-pointer" to='/#about'>
-                            Tools
+                            Home
                         </HashLink>
                     </p>
                     <p>
                         <HashLink className="cursor-pointer" to='/#tools'>
-                            About
+                            About Us
                         </HashLink>
                     </p>
                     <p>
                         <HashLink className="cursor-pointer" to='/#projects'>
-                            Projects
+                            What We Do
                         </HashLink>
                     </p>
                     <p>
                         <HashLink className="cursor-pointer" to='/#testimonials'>
-                            Testimonials
+                            Meet Our Staff
                         </HashLink>
                     </p>
                     <p>
-                        <a href='https://drive.google.com/file/d/1WeurDP0scU81V7QawRa7VlugkPBZfQta/view?usp=sharing'>Download Resume</a>
+                        <HashLink className="cursor-pointer" to='/#testimonials'>
+                            Contact Us
+                        </HashLink>
                     </p>
                 </div>
 
-                <div className='mx-auto'>
+                <div className=''>
                 <h5 className='mb-1 text-lg font-semibold'>
-                    Connect
+                    Other Links
                 </h5>
                     <a href="https://blog.segunajibola.com">
                         Blog
@@ -95,12 +96,10 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <p className='text-lg font-medium tracing-wider my-5'>Built with <span class="text-red-700 text-2xl">🧡</span> by Segun Ajibola.</p>
             <Link to="/" className="flex justify-center">
                 <button className='text-gray-100 border-2 hover:text-gray-800 hover:bg-gray-100 hover:border-gray-200 px-4 py-2 md:my-2 dark:text-gray-900 dark:border-gray-500 dark:hover:bg-gray-300 dark:hover:border-gray-800' onClick={() => window.scrollTo(0, 0)}>
                 Back to Top <FaArrowUp className='inline'/></button>
             </Link>
-        </div>
     <p className='p-2 '>
     Copyright &copy; {new Date().getFullYear()}. All right reserved.
     </p>
