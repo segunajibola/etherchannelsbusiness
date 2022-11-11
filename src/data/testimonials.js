d6f921c7ee4b62a4b4df33cb81907e3679677eb1
Segun Ajibola <ajbl.segun@gmail.com>
2022-11-11 16:15:22 +0100

    home

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -3,7 +3,7 @@ const testimonials = [
       id: 1,
       image: "/images/businessman.png",
       alt: "Business man 1",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime eligendi tempora tempore, consequuntur nihil, libero tenetur sed nesciunt, unde accusantium! Ea sint saepe quaerat quia est eius fugit.",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime fugit.",
       name: "John Doe",
       position: "Manager, LASU CBT",
     },
@@ -11,7 +11,7 @@ const testimonials = [
       id: 2,
       image: "/images/businessman2.png",
       alt: "Business man 2",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime eligendi tempora tempore, consequuntur nihil, libero tenetur sed nesciunt, unde accusantium! Ea sint saepe quaerat quia est eius fugit.",
+      text: "Lorem ipsum dolor sit amdipisci maxime eligendi tempora tempore, consequuntur nihil",
       name: "John Doe",
       position: "Mobile Engineer, ABL",    
     }
