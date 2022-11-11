f6091724b002ad48291787ddee92e1e32fa5b8c8
Segun Ajibola <ajbl.segun@gmail.com>
2022-11-12 00:59:55 +0100

    fixTestimonials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -51,23 +51,32 @@ const Footer = () => {
                 </div>
 
                 <div className=''>
-                <h5 className='mb-1 text-lg font-semibold'>
+                <h5 className='text-xl underline font-semibold'>
                     Other Links
                 </h5>
-                    <a href="https://blog.segunajibola.com">
+                    <a href="" className='block'>
                         Blog
                     </a>
-                    <a href="https://calendly.com/segunajibola/30min" className='mt-[1px] block'>
+                    <a href="" className='block'>
+                        Book A Tour
+                    </a>
+                    <a href="" className='block'>
+                        Privacy Policy
+                    </a>
+                    <a href="" className='block'>
+                        Term & Conditions
+                    </a>
+                    <a href="https://calendly.com/a/30min" className='mt-[1px] block'>
                         Schedule a Meeting
                     </a>
                     <div className='flex gap-1.5 mt-2'>
-                        <a href='https://www.twitter.com/iamsegunajibola'>
+                        <a href='https://www.twitter.com/iama'>
                             <FaTwitter size={24} className='text-blue-500' />
                         </a>
-                        <a href='https://www.linkedin.com/in/segunajibola/'>
+                        <a href='https://www.linkedin.com/in/a/'>
                             <FaLinkedin size={24} className='text-blue-500' />
                         </a>
-                        <a href='https://www.github.com/segunajibola'>
+                        <a href='https://www.github.com/a'>
                             <FaGithub size={24} className='text-white dark:text-gray-800' />
                         </a>
                         <a href='https://wa.me/'>
@@ -78,19 +87,19 @@ const Footer = () => {
                         </a>
                     </div>
                     <div className='flex gap-1 mt-2'>
-                        <a href='https://www.instagram.com/iamsegunajibola'>
+                        <a href='https://www.instagram.com/iama'>
                             <FaInstagram size={24} className='text-red-300 dark:text-red-900' />
                         </a>
                         <a href='mailto:ajbl.segun@gmail.com'>
                             <HiOutlineMail size={29} className='-mt-[2px] text-gray-300 dark:text-gray-600' />
                         </a>
-                        <a href='https://codepen.io/iamsegunajibola'>
+                        <a href='https://codepen.io/iama'>
                             <FaCodepen size={25} className='mt-[1px] text-gray-600' />
                         </a>
-                        <a href='https://stackoverflow.com/users/15411958/segunajibola'>
+                        <a href='https://stackoverflow.com/users/15411958/a'>
                             <FaStackOverflow size={26} className='text-yellow-300 dark:text-yellow-800' />
                         </a>
-                        <a href='https://dev.to/iamsegunajibola'>
+                        <a href='https://dev.to/iama'>
                             <FaDev size={25} className='mt-[2px] text-gray-500' />
                         </a>
                     </div>
