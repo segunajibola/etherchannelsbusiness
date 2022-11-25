4983bb78be40ffa6ddea8d7ea7ae0401e08c311c
Segun Ajibola <ajbl.segun@gmail.com>
2022-11-25 16:07:04 +0100

    fixTestimonials

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -91,6 +91,45 @@ const Home = () => {
 
       {/* Services */}
 
+      <div
+        id="services"
+        className="w-full text-gray-300 bg-[#0a192f] dark:bg-gray-100"
+      >
+        <div className="max-w-[950px] mx-auto p-4 flex flex-col justify-center h-full">
+          <div className="">
+            <h2 className="text-center text-2xl font-semibold tracking-wider dark:text-white my-2">
+              Services
+            </h2>
+            <p className="w-11/12 mx-auto">
+              Our dedicated professionals follow due process to deliver quality
+              I.T services that ensures the growth of your business.
+            </p>
+          </div>
+          <div className="grid grid-cols-1 pt-20 md:-mt-5 md:pt-32 md:grid-cols-2 lg:grid-cols-3 gap-6 z-10">
+            {/* {data.filter((item, index) => index < 6).map((project) => (
+           <ProjectCard project={project} />
+          ))} */}
+          </div>
+          <Link to="/services">
+              <button
+                className="m-2 pb-2 text-sm font-semibold underline tracking-widest dark:border-white dark:bg-blue-600 dark:text-white"
+                onClick={() => window.scrollTo(0, 0)}
+              >
+                here.
+              </button>
+              <FaArrowRight className="inline" />
+            </Link>
+          <Link to="/allprojects" className="flex justify-center">
+            <button
+              className="text-gray-100 border-2 hover:text-gray-800 hover:bg-gray-100 hover:border-gray-200 px-4 py-3 my-8 dark:text-gray-900 dark:border-gray-500 dark:hover:bg-gray-300 dark:hover:border-gray-800"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              See all projects
+            </button>
+          </Link>
+        </div>
+      </div>
+
       <div className="bg-[#2b4b77] dark:bg-gray-700">
         {/* <div className="relative">
           <img
@@ -105,7 +144,8 @@ const Home = () => {
             Services
           </h2>
           <p className="w-11/12 mx-auto">
-          Our dedicated professionals follow due process to deliver quality I.T services that ensures the growth of your business.
+            Our dedicated professionals follow due process to deliver quality
+            I.T services that ensures the growth of your business.
             <Link to="/services">
               <button
                 className="m-2 pb-2 text-sm font-semibold underline tracking-widest dark:border-white dark:bg-blue-600 dark:text-white"
@@ -118,9 +158,7 @@ const Home = () => {
           </p>
         </div>
 
-        <div className="grid md:grid-cols-3">
-        
-        </div>
+        <div className="grid md:grid-cols-3"></div>
       </div>
 
       <div className="mx-auto max-w-[1000px] px-2 py-2 lg:max-w-full lg:px-8">
