4f03c344e888aff7f898306e61f59a469406eca2
Segun Ajibola <ajbl.segun@gmail.com>
2022-11-29 16:44:23 +0100

    fixNav

--- src/data/services.js
+++ src/data/services.js
@@ -1,14 +1,14 @@
 const data = [
   {
     id: 1,
-    image: "/images/homepage1.jpg",
+    image: "/images/itTraining1.jpeg",
     alt: "IT Training",
     name: "IT Training",
     description: "We work with our customers to know the best IT Training courses to recommend that will boost their employees performance and growth with the mind-blowing training facilities in place. We offer Entry Level Training ,Corporate, Professional and Career Builder training options to our customers at both On-site and Off-site.",
     link: "", 
   },{
     id: 2,
-    image: "/images/homepage1.jpg",
+    image: "/images/cbtTesting1.jpeg",
     alt: "CBT Testing",
     name: "CBT Testing",
     description: "We've structured our testing environment at Ether-channel in such a way that would be comfortable, conducive, stress free and secure for the candidates to take their tests with the adequate guidelines and instructions from our competent test administrator/supervisor which helps the students to come out at their very best.",
@@ -16,7 +16,7 @@ const data = [
   },
   {
     id: 3,
-    image: "/images/homepage2.jpg",
+    image: "/images/studyAbroad1.webp",
     alt: "Study Abroad Processing & Immigration",
     name: "Study Abroad Processing & Immigration",
     description: "This is a dummy text, will be changed later to the description of the services.",
@@ -24,7 +24,7 @@ const data = [
   },
   {
     id: 4,
-    image: "/images/homepage3.jpg",
+    image: "/images/networkDeploy1.webp",
     alt: "Network Deployment",
     name: "Network Deployment",
     description: "This is a dummy text, will be changed later to the description of the services.",
@@ -32,7 +32,7 @@ const data = [
   },
   {
     id: 5,
-    image: "/images/homepage4.jpg",
+    image: "/images/cctvInstallation1.jpeg",
     alt: "CCTV Installation",
     name: "CCTV Installation",
     description: "This is a dummy text, will be changed later to the description of the services.",
@@ -40,7 +40,7 @@ const data = [
   },
   {
     id: 6,
-    image: "/images/homepage6.jpg",
+    image: "/images/webDesign1.jpeg",
     alt: "Web Development",
     name: "Web Development",
     description: "This is a dummy text, will be changed later to the description of the services.",
