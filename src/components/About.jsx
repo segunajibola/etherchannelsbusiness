5c9c62ee2ab1a23fefe8e629efd9d80906e9d0f0
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-01 23:40:07 +0100

    fixNav

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import banner from "../assets/about/about2.jpeg"
+import banner from "../assets/about/about2.jpeg";
 
 const About = () => {
   return (
@@ -16,30 +16,30 @@ const About = () => {
             About Us
           </h2>
           <p className="w-11/12 mx-auto my-2 text-xl">
-            Ether-channel Solution Services is an I.T firm founded on the 21st
-            of December 2020, which aimed at providing quality assurance with a
-            top-notch I.T solutions to all our business partners and customers.
-            We are innovative, highly motivated and well equipped with one of
-            the best IT infrastructures in place and quality training hands. We
-            are absolutely committed to ensure that our clients are making good
-            technical decisions and are realizing business value and results
-            from their I.T. investments throughout our business partnering.
+            We are IT Solution Services firm founded on the 21st of December
+            2020 and Our core value is aimed at providing quality assurance with
+            an unbeatable solutions driven to all our business partners and
+            customers. We are innovative, highly motivated and well equipped
+            with one of the best IT infrastructures, quality and competent
+            training hands. We are absolutely committed to ensure that our
+            clients are taking highly recommended technical decisions and are
+            realizing business value and results from their IT Investments
+            throughout our business partnering.
           </p>
         </div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 w-11/12 mx-auto text-center my-16 gap-4">
         <div className="my-3">
-          <h2 className="text-2xl py-8 font-semibold">OUR MISSION CONTENTS</h2>
+          <h2 className="text-2xl py-8 font-semibold">OUR MISSION</h2>
           <p className="text-xl">
-            Our primary aim and objectives is to bridge the technology gap in Nigeria and to motivate and blend young talented tech minds to provides quality, cost-effective training and services to increase individual and organizational productivity and enrichment.
+          Our primary aim and objectives is to bridge the technology gap in Nigeria and to motivate and groom young talented tech minds in providing quality, cost-effective training and digital solution services to increase individual and organizational productivity and enrichment.
           </p>
         </div>
         <div className="mb-3 md:my-3">
-          <h2 className="text-2xl py-8 font-semibold">OUR VISION CONTENTS</h2>
+          <h2 className="text-2xl py-8 font-semibold">OUR VISION</h2>
           <p className="text-xl">
-            Our vision is to be the best and foremost provider of
-            Industry-leading Technological and Business skill set training institute in Nigeria and all around the globe.
+          Our vision is to be the best and foremost provider of Industry-lead Technological and Business skills set training institute in Nigeria and all across the Africa.
           </p>
         </div>
       </div>
