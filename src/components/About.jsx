f334ef1f8bed65d2008daee582e1cf726ac6d102
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-01 23:33:54 +0100

    fixNav

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,21 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import banner from "../assets/about/about2.jpeg"
 
 const About = () => {
   return (
-    <div className="bg-gray-100 dark:bg-gray-700">
-      <div className="grid lg:grid-cols-2">
+    <div className="bg-gray-100 dark:bg-gray-700 py-4">
+      <div className="grid grid-cols-2 md:grid-cols-1 mt-[80px]">
         <img
-          src="/images/aboutUs1.png"
+          src={banner}
           alt=""
-          className="mt-0 mb-3 w-screen mx-2 h-[320px] mx-auto"
+          className="mb-3 w-screen h-[70vh] object-cover object-center"
         />
         <div className="mx-auto text-center">
-          <h2 className="text-2xl font-semibold tracking-wider text-gray-900 dark:text-white my-2">
+          <h2 className="text-3xl font-semibold tracking-wider text-gray-900 dark:text-white my-4">
             About Us
           </h2>
-          <p className="w-11/12 mx-auto my-2">
+          <p className="w-11/12 mx-auto my-2 text-xl">
             Ether-channel Solution Services is an I.T firm founded on the 21st
             of December 2020, which aimed at providing quality assurance with a
             top-notch I.T solutions to all our business partners and customers.
@@ -27,16 +28,16 @@ const About = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 w-11/12 mx-auto text-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 w-11/12 mx-auto text-center my-16 gap-4">
         <div className="my-3">
-          <h2>OUR MISSION CONTENTS</h2>
-          <p>
+          <h2 className="text-2xl py-8 font-semibold">OUR MISSION CONTENTS</h2>
+          <p className="text-xl">
             Our primary aim and objectives is to bridge the technology gap in Nigeria and to motivate and blend young talented tech minds to provides quality, cost-effective training and services to increase individual and organizational productivity and enrichment.
           </p>
         </div>
         <div className="mb-3 md:my-3">
-          <h2>OUR VISION CONTENTS</h2>
-          <p>
+          <h2 className="text-2xl py-8 font-semibold">OUR VISION CONTENTS</h2>
+          <p className="text-xl">
             Our vision is to be the best and foremost provider of
             Industry-leading Technological and Business skill set training institute in Nigeria and all around the globe.
           </p>
