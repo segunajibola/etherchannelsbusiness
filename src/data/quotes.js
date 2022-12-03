4afc6ddf97090f0ca9ebb89f1d9ccabc7b0d68c2
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-03 23:06:37 +0100

    aboutPage

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -10,32 +10,77 @@ const quotes = [
       name: "Billy Cox"
     },
     {
-      id: 2,
+      id: 3,
       text: '“Technology is just a tool. In terms of getting the kids working together and motivating them, the teacher is the most important.”',
       name: "Bill Gates"
     },
     {
-      id: 2,
+      id: 4,
       text: '“Our business is about technology, yes. But it’s also about operations and customer relationships.”',
       name: "Michael Dell"
     },
     {
-      id: 2,
+      id: 5,
       text: '“What new technology does is create new opportunities to do a job that customers want done.”',
       name: "Tim O’Reilly"
     },
     {
-      id: 2,
+      id: 6,
       text: '“Technology like art is a soaring exercise of the human imagination.”',
       name: "Daniel Bell"
     },
     {
-      id: 2,
+      id: 7,
+      text: '“It has become appallingly obvious that our technology has exceeded our humanity.”',
+      name: "Albert Einstein"
+    },
+    {
+      id: 8,
+      text: '“One machine can do the work of fifty ordinary men. No machine can do the work of one extraordinary man.”',
+      name: "Elbert Hubbard"
+    },
+    {
+      id: 9,
+      text: '“Technology made large populations possible; large populations now make technology indispensable.” ',
+      name: "Joseph Wood Krutch"
+    },
+    {
+      id: 10,
+      text: '“Just because something doesn’t do what you planned it to do doesn’t mean it’s useless.”',
+      name: "Thomas Edison"
+    },
+    {
+      id: 11,
+      text: '“The science of today is the technology of tomorrow.”',
+      name: "Edward Teller"
+    },
+    {
+      id: 12,
       text: '',
       name: ""
     },
     {
-      id: 2,
+      id: 13,
+      text: '',
+      name: ""
+    },
+    {
+      id: 12,
+      text: '',
+      name: ""
+    },
+    {
+      id: 13,
+      text: '',
+      name: ""
+    },
+    {
+      id: 12,
+      text: '',
+      name: ""
+    },
+    {
+      id: 13,
       text: '',
       name: ""
     },
