28f6091fd8e297eb83d32750ca1ca0c53ffbaa24
Segun Ajibola <ajbl.segun@gmail.com>
2023-01-23 16:02:46 +0100

    addCorporateTraining

--- src/components/ParticularService.jsx
+++ src/components/ParticularService.jsx
@@ -32,215 +32,220 @@ const ParticularService = ({ services }) => {
               <h1 className="pt-10 text-3xl font-semibold">{services.name}</h1>
               <p className="px-12 py-4 text-lg">{services.description}</p>
               <h1 className="text-2xl pt-[150px] pb-8">{services.heading0}</h1>
-
               {/* CBT Testing */}
-              {services.heading1 && <div className="bg-4">
-                <p className="font-bold py-4 text-2xl text-white shadow-xl pt-10 pb-16">
-                  {services.heading1}
-                </p>
-                <div
-                  className="flex w-[80vw] mx-auto text-center gap-6 pb-24 justify-center"
-                  data-aos="zoom-in"
-                >
-                  <div>
-                    <img
-                      src={services.firstpic1}
-                      className="w-44 h-44 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.firstname1}</p> */}
+              {services.heading1 && (
+                <div className="bg-4">
+                  <p className="font-bold py-4 text-2xl text-white shadow-xl pt-10 pb-16">
+                    {services.heading1}
+                  </p>
+                  <div
+                    className="flex w-[80vw] mx-auto text-center gap-6 pb-24 justify-center"
+                    data-aos="zoom-in"
+                  >
+                    <div>
+                      <img
+                        src={services.firstpic1}
+                        className="w-44 h-44 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.firstname1}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.firstpic2}
+                        className="w-52 h-44 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.firstname2}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.firstpic3}
+                        className="w-52 h-44 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.firstname3}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.firstpic4}
+                        className="w-52 h-44 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.firstname4}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.firstpic5}
+                        className="w-52 h-44 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.firstname5}</p> */}
+                    </div>
                   </div>
-                  <div>
-                    <img
-                      src={services.firstpic2}
-                      className="w-52 h-44 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.firstname2}</p> */}
+                  <p className="font-bold py-4 text-2xl text-white pt-10 pb-16">
+                    {services.heading2}
+                  </p>
+                  <div
+                    className="flex w-[80%] mx-auto text-center gap-5 pb-24 justify-center"
+                    data-aos="zoom-right"
+                  >
+                    <div>
+                      <img
+                        src={services.secondpic1}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.secondname1}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.secondpic2}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.secondname2}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.secondpic3}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.secondname3}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.secondpic4}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.secondname4}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.secondpic5}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.secondname5}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.secondpic6}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.secondname6}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.secondpic7}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.secondname7}</p> */}
+                    </div>
                   </div>
-                  <div>
-                    <img
-                      src={services.firstpic3}
-                      className="w-52 h-44 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.firstname3}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.firstpic4}
-                      className="w-52 h-44 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.firstname4}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.firstpic5}
-                      className="w-52 h-44 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.firstname5}</p> */}
-                  </div>
-                </div>
-                <p className="font-bold py-4 text-2xl text-white pt-10 pb-16">
-                  {services.heading2}
-                </p>
-                <div
-                  className="flex w-[80%] mx-auto text-center gap-5 pb-24 justify-center"
-                  data-aos="zoom-right"
-                >
-                  <div>
-                    <img
-                      src={services.secondpic1}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.secondname1}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.secondpic2}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.secondname2}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.secondpic3}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.secondname3}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.secondpic4}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.secondname4}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.secondpic5}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.secondname5}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.secondpic6}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.secondname6}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.secondpic7}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.secondname7}</p> */}
-                  </div>
-                </div>
-                {/* </div> */}
-                <p className="font-bold py-4 text-2xl text-white pt-10 pb-16">
-                  {services.heading3}
-                </p>
-                <div
-                  className="flex w-[80%] mx-auto text-center gap-5 pb-24 justify-center"
-                  data-aos="zoom-right"
-                >
-                  <div>
-                    <img
-                      src={services.thirdpic1}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.thirdname1}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.thirdpic2}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.thirdname2}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.thirdpic3}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.thirdname3}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.thirdpic4}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.thirdname4}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.thirdpic5}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.thirdname5}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.thirdpic6}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.thirdname6}</p> */}
-                  </div>
-                  <div>
-                    {/* <img
+                  {/* </div> */}
+                  <p className="font-bold py-4 text-2xl text-white pt-10 pb-16">
+                    {services.heading3}
+                  </p>
+                  <div
+                    className="flex w-[80%] mx-auto text-center gap-5 pb-24 justify-center"
+                    data-aos="zoom-right"
+                  >
+                    <div>
+                      <img
+                        src={services.thirdpic1}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.thirdname1}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.thirdpic2}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.thirdname2}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.thirdpic3}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.thirdname3}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.thirdpic4}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.thirdname4}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.thirdpic5}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.thirdname5}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.thirdpic6}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.thirdname6}</p> */}
+                    </div>
+                    <div>
+                      {/* <img
                     src={services.thirdpic7}
                     className="w-36 h-36 border-4 border-blue-200 p-2"
                     alt=""
                     srcset=""
                   /> */}
-                    {/* <p>{services.thirdname7}</p> */}
-                  </div>
-                  <div>
-                    <img
-                      src={services.thirdpic8}
-                      className="w-36 h-36 border-4 border-blue-200 p-2"
-                      alt=""
-                      srcset=""
-                    />
-                    {/* <p>{services.thirdname8}</p> */}
+                      {/* <p>{services.thirdname7}</p> */}
+                    </div>
+                    <div>
+                      <img
+                        src={services.thirdpic8}
+                        className="w-36 h-36 border-4 border-blue-200 p-2"
+                        alt=""
+                        srcset=""
+                      />
+                      {/* <p>{services.thirdname8}</p> */}
+                    </div>
                   </div>
                 </div>
-              </div>}
+              )}
+
+              <div>{services.corporatetraininga}</div>
+              <div>{services.corporatetrainingb}</div>
+              <div>{services.corporatetrainingc}</div>
 
               <div className="bg-gray-300">
                 <h2 className="pt-24 pb-8 font-bold text-2xl">
