98d18f10aa589ce5fa0456a9576f421c3a6a0f39
Segun Ajibola <ajbl.segun@gmail.com>
2023-03-14 16:28:22 +0100

    changeLayout

--- app/page.jsx
+++ app/page.jsx
@@ -4,22 +4,14 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 import { useState } from "react";
-import { Navbar, Hero, Footer } from "../components";
+import { Hero } from "../components";
 
 // const inter = Inter({ subsets: ['latin'] })
 
 const Home = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const handleMode = () => {
-    setDarkMode(!darkMode);
-  };
   return (
     <div>
-      {/* <Navbar handleMode={handleMode} darkMode={darkMode} /> */}
-      <div>
-        <Hero />
-      </div>
-      {/* <Footer /> */}
+      <Hero />
     </div>
   );
 };
